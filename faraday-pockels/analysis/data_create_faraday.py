6132ab242148b0c098f798c2d89acea9a80a8d1c
vsilv <vsilverlord@googlemail.com>
2014-09-19 19:11:43 +0200

    minor changes

---
+++ faraday-pockels/analysis/data_create_faraday.py
@@ -0,0 +1,49 @@
+import numpy as np
+# transform angle 17x degrees into negative angles
+input_dir = "data_faraday/"
+output_dir = input_dir
+for i in range(4):
+    f = open(input_dir + "2.%i.csv"%(i+1))   # read data from csv table
+    a = []
+    for line in f:
+        a += [float(line)]
+    f.close()
+    a = np.array(a)
+    if i == 3:
+        for k, I in enumerate(a):
+            if I > 90:  # dont take 'positive' values
+                a[k] = I - 180
+    np.save(output_dir + "a_%i"%i, a)
+
+# create np arrays for I
+Imax = 4.8
+I = np.arange(0,Imax+0.2,0.2)
+I = np.append(I, 4.94)
+np.save(output_dir + "i_1", I)
+
+Imax = -4.8
+I = np.arange(0,Imax-0.2,-0.2)
+I = np.append(I, -4.90)
+np.save(output_dir + "i_2", I)
+
+# i_3.npy is already existing (random numbers)
+
+Imax = 4.60
+I = np.arange(0,Imax+0.2,0.2)
+I = np.append(I, 4.70)
+np.save(output_dir + "i_4", I)
+
+
+# create random values for I, only for measuremente 2.3
+"""
+if i == 3:
+    create = 0
+    if create:
+        Imax = 5
+        n = 27
+        np.random.seed(5)
+        rands = np.random.random(25) * 5
+        a = np.round(np.sort(rands), decimals=2)
+        np.save("2.3_rand_i", a)
+        print(["%.2f"%k for k in a])
+"""
