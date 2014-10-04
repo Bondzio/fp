010255e80a8f5e2d5af76cd256293d71309dea74
alfred-s <f.schuessler@posteo.de>
2014-10-04 15:41:07 +0200

    analysis

--- faraday-pockels/analysis_pockels/refined_analysis_fred.py
+++ faraday-pockels/analysis_pockels/refined_analysis_fred.py
@@ -37,15 +37,22 @@ def save_all():
 def acf(x, length=20):
     return np.array([1]+[np.corrcoef(x[:-i], x[i:])[0,1] for i in range(1, length)])
 
-
+############################################################################################################
 # Two subplots, unpack the axes array immediately
+show_fig = 1
+save_fig = 1
 plt.close('all')
+fig_dir = '../figures/'
+
 # Examplary cases: k = 8, k = 12
 ks = range(8, 19)       # plots
+U_DC = [135.5,137.0, 138.0,138.5,139.0,139.5,140.0,140.5,141.0,142.0,144.0]
 k_example = 9
 freq1 = np.zeros(len(ks))
 freq2 = np.zeros(len(ks))
-f, axarr = plt.subplots(1,2, figsize = (12,5))
+fig1, axarr = plt.subplots(1,2, figsize = (12,5))
+fig2, ax2 = plt.subplots(1, 1, figsize = (12,7))
+fig3, ax3 = plt.subplots(1, 2, figsize = (12,6))
 for i, k in enumerate(ks):
     U1 = np.load("npy/U1_%03d.npy"%k)
     U2 = np.load("npy/U2_%03d.npy"%k)
@@ -74,47 +81,31 @@ for i, k in enumerate(ks):
     freq2[i] = psd_U2[8]
 
     if k == k_example:
-        f3, ax3 = plt.subplots(1, 1, figsize = (15,11))
-        f3.suptitle('Examplary plot for k = %i'%k)
+        if not save_fig:
+            fig2.suptitle('Examplary plot for $U_\mathrm{DC} = %.1f$'%U_DC[i])
+            fig3.suptitle('Fourier transform for $U_\mathrm{DC} = %.1f$'%U_DC[i])
         ymin = -0.015
         ymax = 0.025
-        ax3.plot(t, U2, label='$U_\mathrm{out}(t)$')                # plots of cut-off output
-        ax3.plot(t_old, U1*0.2, label='$0.2 U_\mathrm{in}(t)$')                          # plotting the incoming signal
-        ax3.plot(t_old[[lower, lower]], [ymin, ymax], 'k--', label='cut-off') # plotting the cut-offs
-        ax3.plot(t_old[[upper, upper]], [ymin, ymax], 'k--') # plotting the cut-offs
-        ax3.legend(loc=4)
-        ax3.set_ylim([ymin, ymax])
-        ax3.set_xlabel('$t$')
-        ax3.set_ylabel('$U(t)$')
-        ax3.legend()
+        ax2.plot(t, U2, label='$U_\mathrm{out}(t)$')                # plots of cut-off output
+        ax2.plot(t_old, U1*0.2, label='$0.2 \cdot U_\mathrm{in}(t)$')                          # plotting the incoming signal
+        ax2.plot(t_old[[lower, lower]], [ymin, ymax], 'k--', label='$\mathrm{cut-off}$') # plotting the cut-offs
+        ax2.plot(t_old[[upper, upper]], [ymin, ymax], 'k--') # plotting the cut-offs
+        ax2.legend(loc=4)
+        ax2.set_ylim([ymin, ymax])
+        ax2.set_xlabel('$t$')
+        ax2.set_ylabel('$U(t)$')
+        ax2.legend()
 
-        f4, ax4 = plt.subplots(1, 2, figsize = (11,6))
-        f4.suptitle('Fourier transform for k = %i'%k)
-        ax4[0].semilogy(f, psd_U2)           
-        ax4[0].set_xlim([0, 70])
-        ax4[0].set_xlabel('$\\nu$')
-        ax4[0].set_ylabel('$\mathrm{|FFT(U_\mathrm{out})|^2(\\nu)}$')
-        ax4[1].semilogy(f, psd_U2)           
-        ax4[1].set_xlim([0, 2])
-        ax4[1].set_ylim([10**-7, 0.05])
-        ax4[1].set_xlabel('$\\nu$')
-        ax4[1].set_ylabel('$\mathrm{|FFT(U_\mathrm{out})|^2(\\nu)}$')
-"""
-    mx = 4
-    my = 3
-    m = mx * my
-    if i % m == 0:
-        f2, axarr2 = plt.subplots(mx, my, figsize = (15,11))
-        f2.suptitle('Plots for all k')
-    ax = axarr2[int(i%m/my), (i%m)%my]
-    ax.plot(t, U2, label='k = %i'%k)                # plots of cut-off output
-    ax.plot(t_old, U1*0.2)                          # plotting the incoming signal
-    ax.plot(t_old[[lower, upper]], U1[[lower, upper]]*0.2, 'o') # plotting the cut-offs
-    ax.legend(loc=4)
-"""
+        ax3[0].semilogy(f, psd_U2)           
+        ax3[0].set_xlim([0, 70])
+        ax3[0].set_xlabel('$\\nu$')
+        ax3[0].set_ylabel('$\mathrm{|FFT(U_\mathrm{out})|^2(\\nu)}$')
+        ax3[1].semilogy(f, psd_U2)           
+        ax3[1].set_xlim([0, 2])
+        ax3[1].set_ylim([10**-7, 0.05])
+        ax3[1].set_xlabel('$\\nu$')
+        ax3[1].set_ylabel('$\mathrm{|FFT(U_\mathrm{out})|^2(\\nu)}$')
 
-
-axarr[0].set_title('Fourier Transform')
 axarr[0].plot([f[4], f[4]], [10**-7, 1], 'k-.', label='$\\nu_1 = %.2f\, \mathrm{kHz}$'%(f[4]))
 axarr[0].plot([f[8], f[8]], [10**-7, 1], 'k--', label='$\\nu_1 = %.2f\, \mathrm{kHz}$'%(f[8]))
 axarr[0].set_xlim([0, 2])
@@ -123,30 +114,24 @@ axarr[0].set_xlabel('$\\nu$')
 axarr[0].set_ylabel('$\mathrm{|FFT(U_\mathrm{out})|^2(\\nu)}$')
 axarr[0].legend(loc=4)
 
-axarr[1].semilogy(ks, freq1, 'b.', label='$\\nu_1 = %.2f\, \mathrm{kHz}$'%(f[4]))
-axarr[1].semilogy(ks, freq2, 'r.', label='$\\nu_2 = %.2f\, \mathrm{kHz}$'%(f[8]))
-axarr[1].set_title('Amplitude at %.2f and %.2f kHz'%(f[4], f[8]))
+axarr[1].semilogy(U_DC, freq1, 'b.', label='$\\nu_1 = %.2f\, \mathrm{kHz}$'%(f[4]))
+axarr[1].semilogy(U_DC, freq2, 'r.', label='$\\nu_2 = %.2f\, \mathrm{kHz}$'%(f[8]))
 axarr[1].set_ylim([10**-4, 0.05])
-axarr[1].set_xlabel('$k$')
+axarr[1].set_xlabel('$U_\mathrm{DC}$')
 axarr[0].set_ylabel('$\mathrm{|FFT(U_\mathrm{out})|^2(\\nu_i)}$')
 axarr[1].legend(loc=4)
+if not save_fig:
+    axarr[0].set_title('PSD for all U_DC')
+    axarr[1].set_title('Amplitude at %.2f and %.2f kHz'%(f[4], f[8]))
 
+if show_fig:
+    fig1.show()
+    fig2.show()
+    fig3.show()
+if save_fig: 
+    fig1.savefig(fig_dir + "fourier_all.pdf")
+    fig2.savefig(fig_dir + "cut_off_example.pdf")
+    fig3.savefig(fig_dir + "fft_example.pdf")
 
-for i, k in enumerate(ks):
-    # Using the auto correlation function of U2
-    N2 = N-2
-    U2acf = acf(U2,N2)
-    w2   = blackman(N2)
-    w2 = np.ones(N2)
-    U2acff = fft(U2acf*w2) 
-    tf2 = np.linspace(0.0, 1.0/(2.0*T), (N2)/2)
-'''
-    axarr[1, 0].plot(t[2:],U2acf)
-    axarr[1, 0].set_title('Autocorrelation function')
-
-    axarr[1, 1].semilogy(tf2, 2.0 / N2 * np.abs(U2acff[0:N2/2])**2)
-    axarr[1, 1].set_title('Fourier transform of Autocorrelation function')
-'''
-plt.show()
 
 
