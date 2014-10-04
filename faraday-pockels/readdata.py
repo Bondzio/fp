79b3eb3dced640c4038e277139743392898f93be
vsilv <vsilverlord@googlemail.com>
2014-10-04 15:14:26 +0200

    minor

--- faraday-pockels/readdata.py
+++ faraday-pockels/readdata.py
@@ -8,53 +8,33 @@ def table(x,y,name,title):
 
     q = """
     \\begin{table}[htdp]"""
-    for l in range(Ny-1):
+    for l in range(Ny):
+        endrange   =  Nx*(l+1)
+        alpha = Nx
+        if l==(Ny-1):
+            alpha = J%Nx 
+            endrange = Nx*l + alpha
         q+="""
         \\begin{tabular}{|l|%s|}
-        \\hline"""%(("|p{%s}"%length)*Nx)
-
+        \\hline"""%(("|p{%s}"%length)*alpha)
         if l==0: 
             q+="""
             \\multicolumn{%d}{|c|}{\\cellcolor[RGB]{206,250,201}$
-            \\mathbf{%s}$} \\\\\n"""%(Nx+1,title)
-
-        q += ("\\textbf{%s}"%(name[0])+"& %.2f"*Nx+" \\\\\n")%(tuple(x[Nx*l:Nx*(l+1)]))
-        q += ("\\textbf{%s}"%(name[1])+"& %.2f"*Nx+" \\\\\n")%(tuple(y[Nx*l:Nx*(l+1)]))
+            \\mathbf{%s}$} \\\\\n"""%(alpha+1,title)
+        q += ("\\textbf{%s}"%(name[0])+"& %.2f"*alpha+" \\\\\n")%(tuple(x[Nx*l:endrange]))
+        q += ("\\textbf{%s}"%(name[1])+"& %.2f"*alpha+" \\\\\n")%(tuple(y[Nx*l:endrange]))
         q+="""
         \\hline
         \\end{tabular}"""
-
-    # last one
-    l = Ny-1
-
-    rest = J%Nx 
-    hinten = Nx - rest -1
-
-    q+="""
-    \\begin{tabular}{|l|%s|}
-    \\hline"""%(("|p{%s}"%length)*rest)
-
-    if l==0: 
-        q+="""
-        \\multicolumn{%d}{|c|}{\\cellcolor[RGB]{206,250,201}$
-        \\mathbf{%s}$} \\\\\n"""%(rest+1,title)
-
-    q += ("\\textbf{%s}"%(name[0])+"& %.2f"*rest+" \\\\\n")%(tuple(x[Nx*l:(Nx*l+rest)]))
-    q += ("\\textbf{%s}"%(name[1])+"& %.2f"*rest+" \\\\\n")%(tuple(y[Nx*l:(Nx*l+rest)]))
-    q+="""
-    \\hline
-    \\end{tabular}"""
-
     q+="""
     \\caption{}
-    \\label{Power05}
+    \\label{}
     \\end{table}"""
     print(q)
 
-
 input_dir = "data_faraday/"
 
-for i in range(4):
+for i in range(1):
     a = np.load(input_dir +"a_%i.npy"%(i+1))
     I = np.load(input_dir + "i_%i.npy"%(i+1))
     names = ["angle $\\alpha$", "Current $I$"]
