cc7086ab521d10580ec33a37c96b47f4a0a0786d
vsilv <vsilverlord@googlemail.com>
2014-10-14 18:13:39 +0200

    locking method

--- shl/analysis/analysis.py
+++ shl/analysis/analysis.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 
 #fredsche routinen
-from stat import *
+import stat1 as st
 
 
 import seaborn as sns
@@ -81,12 +81,14 @@ def plot_2_1():
 
 from scipy.interpolate import UnivariateSpline,InterpolatedUnivariateSpline,interp1d
 from smooth import savitzky_golay
+
 def plot_4_1():
     data = np.load("./data/measure4_1.npy")[2:]
     c    = np.where(data!=0)
     x = np.arange(0,len(data),1)
-
     x = x[c]
+    x = 1.3149710372035508*x -22.617788714272098
+    
     data = data[c]
 
     fig = plt.figure()
@@ -107,14 +109,16 @@ def plot_4_1():
 
     plt.ylim(min(data)*0.8,max(data))
     plt.xlim(min(x)*0.8,max(x))
-    plt.xlabel("Channels", fontsize = 14)
+    plt.xlabel("time t in $ns$", fontsize = 14)
     plt.ylabel("counts", fontsize = 14)
 
     ax.xaxis.set_tick_params(labelsize = 14)
     ax.yaxis.set_tick_params(labelsize = 14)
 
 
-    make_fig(fig,1,1,name="plot4_1")
+    make_fig(fig,1,0,name="plot4_1")
+
+plot_4_1()
 
 def plot_4_1_log():
     data = np.load("./data/measure4_1.npy")[2:]
@@ -249,7 +253,6 @@ def plot_5_1_hist():
 
     make_fig(fig,1,1,name="plot5_1_hist")
     plt.show()
-
 def calib_TAC_MCA():
     delay   = np.array([0,8,16,24,32,40,48,56,64,72,80,88,96,104,112,120,128,136,144,152,160,168,176,184,190.5])
     channel = np.array([20,24,29,34,41,47,53,59,66,72,78,84,90,96,102,109,115,121,127,133,139,145,151,157,162])
@@ -261,6 +264,11 @@ def calib_TAC_MCA():
 
 
     p, cov= np.polyfit(channel,delay, 1, full=False, cov=True, w=y_error)
+
+    #f1 = open("coefficients.tex","a")
+    #st.la_coeff(f1, p,cov, ["p_1","p_2"])
+
+
     x_fit = np.linspace(20,162,1000)
 
     data_fit     = (np.polyval(p, x_fit))
@@ -268,22 +276,27 @@ def calib_TAC_MCA():
     data_fit_max = (np.polyval(p + np.sqrt(np.diag(cov)),x_fit))
 
     plt.plot(x_fit,data_fit)
+
+    print(st.chi2_min(channel,delay,p,y_error)/(len(channel)-1))
+
+    
     
     plt.xlim(20,162)
     plt.ylim(0,191)
 
-    plt.ylabel("Delay",fontsize = 14)
+    plt.ylabel("Delay $\Delta t$ in ns",fontsize = 14)
     plt.xlabel("Channel", fontsize = 14)
 
     ax.xaxis.set_tick_params(labelsize = 14)
     ax.yaxis.set_tick_params(labelsize = 14)
 
 
-
     plt.fill_between(x_fit, data_fit_min , data_fit_max,facecolor="r", color="b", alpha=0.3 )
 
     plt.errorbar(channel,delay, yerr= y_error,xerr= x_error, fmt= "x")
-    make_fig(fig,1,1,name = "plot7")
+    make_fig(fig,1,0,name = "plot7")
+
+calib_TAC_MCA()
 
 def calib_TAC_MCA2():
     delay   = np.array([25,108.5,162.,0.5,65.5,113.5,75.5,49,79,135.5,23.5,98.5,135.5,110.5,121,127,45,51.5,184,80,185,88.5,52,167.5,44.5])
@@ -297,6 +310,8 @@ def calib_TAC_MCA2():
     plt.errorbar(channel,delay, yerr= y_error,xerr= x_error, fmt= "x")
 
     p, cov= np.polyfit(channel,delay, 1, full=False, cov=True, w=y_error)
+
+    
     x_fit = np.linspace(20,162,1000)
 
     data_fit     = (np.polyval(p, x_fit))
@@ -319,7 +334,6 @@ def calib_TAC_MCA2():
     plt.fill_between(x_fit, data_fit_min , data_fit_max,facecolor="r", color="b", alpha=0.3 )
     make_fig(fig,1,1,name = "plot71")
 
-calib_TAC_MCA()
 def calib_TAC_MCA_rescaled():
     delay   = np.array([0,8,16,24,32,40,48,56,64,72,80,88,96,104,112,120,128,136,144,152,160,168,176,184,190.5])
     channel = np.array([20,24,29,34,41,47,53,59,66,72,78,84,90,96,102,109,115,121,127,133,139,145,151,157,162])
