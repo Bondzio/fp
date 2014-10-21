895f509723b395fe13489738f60bd706b05ea374
vsilv <vsilverlord@googlemail.com>
2014-10-21 16:49:44 +0200

    minor

--- ultrasonic/analysis/theory_plots.py
+++ ultrasonic/analysis/theory_plots.py
@@ -2,18 +2,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as ssn
 
-fontsize_labels = 12    # size used in latex document
-
 from matplotlib import rcParams
 
 rcParams['font.family'] = 'serif'
 rcParams['font.serif'] = ['Computer Modern Roman']
 rcParams['text.usetex'] = True
 rcParams['figure.autolayout'] = True
-rcParams['axes.labelsize'] = fontsize_labels
-rcParams['xtick.labelsize'] = fontsize_labels
-rcParams['ytick.labelsize'] = fontsize_labels
 
+fig_dir = "../figures/"
 
 def use_2pi(fig,ax):
     unit   = 0.25 
@@ -22,21 +18,30 @@ def use_2pi(fig,ax):
     x_label = [r"$-\pi$", r"$- \frac{\pi}{4}$", r"$0$", r"$\frac{\pi}{4}$",   r"$\pi$"]
     ax.set_xticks(x_tick*2*np.pi)
     ax.set_xticklabels(x_label, fontsize=20)
+    
+def make_fig(fig, show=True,save=False, name="foo"):
+    if show == True:
+        fig.show()
+    if save == True:
+        fig.savefig(fig_dir + name + ".pdf")
 
 
-lamb = 632e-9
+
+lamb = 632.8e-9
 
 k = 1 / lamb
-theta = np.linspace(-np.pi,np.pi,1000)
+#theta = np.linspace(-np.pi,np.pi,1000)
+x0 = np.linspace(-2,2,1000)
 b = 1e-5
-I = np.sin(k * b/2* np.sin(theta))**2/ (k**2 * np.sin(theta)**2) 
+#I = np.sin(k * b/2* np.sin(theta))**2/ (k**2 * np.sin(theta)**2) 
+I = np.sin(k * b/2* x0)**2/ (k**2 * x0**2) 
 I/=np.sum(I)
 
 fig = plt.figure()
 ax  = plt.subplot(111)
-use_2pi(fig,ax)
-plt.plot(theta, I)
-plt.xlabel(r"angle $\theta$")
+#use_2pi(fig,ax)
+plt.plot(x0, I)
+plt.xlabel(r"Position $x$")
 plt.ylabel(r"density $\mathcal{P}$")
 plt.show()
-
+make_fig(fig, 1 ,1, "sinc1")
